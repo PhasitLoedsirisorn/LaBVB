d50568fb9a4d2bceaa699ae869e9240fb5b835c1
Unknown <gustnmk2011@gmail.com>
2018-12-17 18:00:26 +0700

    11
    11

---
+++ Lab2/Lab2/frmJob4.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmJob4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtToday = New System.Windows.Forms.TextBox()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.txtSale = New System.Windows.Forms.TextBox()
+        Me.lblYearSalary = New System.Windows.Forms.Label()
+        Me.lblBonus = New System.Windows.Forms.Label()
+        Me.lblAllIncome = New System.Windows.Forms.Label()
+        Me.lblAllowance = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radLongDate = New System.Windows.Forms.RadioButton()
+        Me.radGeneralDate = New System.Windows.Forms.RadioButton()
+        Me.radShortDate = New System.Windows.Forms.RadioButton()
+        Me.btnCalTax = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(283, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(8, 0, 8, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(353, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "โปรแกรมคำนวณภาษี(อย่างหยาบ)"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(97, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(159, 37)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "วันที่"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label3.Location = New System.Drawing.Point(97, 158)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(159, 37)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "เงินเดือน"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(97, 208)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(159, 37)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "ยอดขายทั้งปี"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.Location = New System.Drawing.Point(97, 255)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(159, 37)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "เงินเดือนทั้งปี"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label6.Location = New System.Drawing.Point(97, 301)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(159, 37)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "รับเงินโบนัส"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label7.Location = New System.Drawing.Point(97, 351)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(159, 37)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "รวมเงินรายได้"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label8.Location = New System.Drawing.Point(97, 399)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(159, 37)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "หักค่าลดหย่อน"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label9.Location = New System.Drawing.Point(97, 448)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(159, 37)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "ภาษีที่ต้องชำระ"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtToday
+        '
+        Me.txtToday.Location = New System.Drawing.Point(288, 108)
+        Me.txtToday.Name = "txtToday"
+        Me.txtToday.Size = New System.Drawing.Size(392, 35)
+        Me.txtToday.TabIndex = 0
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Location = New System.Drawing.Point(288, 160)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(392, 35)
+        Me.txtSalary.TabIndex = 1
+        '
+        'txtSale
+        '
+        Me.txtSale.Location = New System.Drawing.Point(288, 210)
+        Me.txtSale.Name = "txtSale"
+        Me.txtSale.Size = New System.Drawing.Size(392, 35)
+        Me.txtSale.TabIndex = 2
+        '
+        'lblYearSalary
+        '
+        Me.lblYearSalary.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblYearSalary.Location = New System.Drawing.Point(288, 255)
+        Me.lblYearSalary.Name = "lblYearSalary"
+        Me.lblYearSalary.Size = New System.Drawing.Size(392, 37)
+        Me.lblYearSalary.TabIndex = 3
+        '
+        'lblBonus
+        '
+        Me.lblBonus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBonus.Location = New System.Drawing.Point(288, 303)
+        Me.lblBonus.Name = "lblBonus"
+        Me.lblBonus.Size = New System.Drawing.Size(392, 37)
+        Me.lblBonus.TabIndex = 3
+        '
+        'lblAllIncome
+        '
+        Me.lblAllIncome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAllIncome.Location = New System.Drawing.Point(288, 351)
+        Me.lblAllIncome.Name = "lblAllIncome"
+        Me.lblAllIncome.Size = New System.Drawing.Size(392, 37)
+        Me.lblAllIncome.TabIndex = 3
+        '
+        'lblAllowance
+        '
+        Me.lblAllowance.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAllowance.Location = New System.Drawing.Point(288, 399)
+        Me.lblAllowance.Name = "lblAllowance"
+        Me.lblAllowance.Size = New System.Drawing.Size(392, 37)
+        Me.lblAllowance.TabIndex = 3
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTax.Location = New System.Drawing.Point(288, 450)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(392, 37)
+        Me.lblTax.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radLongDate)
+        Me.GroupBox1.Controls.Add(Me.radGeneralDate)
+        Me.GroupBox1.Controls.Add(Me.radShortDate)
+        Me.GroupBox1.Location = New System.Drawing.Point(726, 158)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(264, 158)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "รูปแบบวันที่"
+        '
+        'radLongDate
+        '
+        Me.radLongDate.AutoSize = True
+        Me.radLongDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.radLongDate.Location = New System.Drawing.Point(28, 105)
+        Me.radLongDate.Name = "radLongDate"
+        Me.radLongDate.Size = New System.Drawing.Size(200, 28)
+        Me.radLongDate.TabIndex = 1
+        Me.radLongDate.TabStop = True
+        Me.radLongDate.Text = "วันที่แบบ Long Date"
+        Me.radLongDate.UseVisualStyleBackColor = True
+        '
+        'radGeneralDate
+        '
+        Me.radGeneralDate.AutoSize = True
+        Me.radGeneralDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.radGeneralDate.Location = New System.Drawing.Point(28, 75)
+        Me.radGeneralDate.Name = "radGeneralDate"
+        Me.radGeneralDate.Size = New System.Drawing.Size(227, 28)
+        Me.radGeneralDate.TabIndex = 1
+        Me.radGeneralDate.TabStop = True
+        Me.radGeneralDate.Text = "วันที่แบบ General Date"
+        Me.radGeneralDate.UseVisualStyleBackColor = True
+        '
+        'radShortDate
+        '
+        Me.radShortDate.AutoSize = True
+        Me.radShortDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.radShortDate.Location = New System.Drawing.Point(28, 46)
+        Me.radShortDate.Name = "radShortDate"
+        Me.radShortDate.Size = New System.Drawing.Size(202, 28)
+        Me.radShortDate.TabIndex = 0
+        Me.radShortDate.TabStop = True
+        Me.radShortDate.Text = "วันที่แบบ Short Date"
+        Me.radShortDate.UseVisualStyleBackColor = True
+        '
+        'btnCalTax
+        '
+        Me.btnCalTax.Location = New System.Drawing.Point(726, 354)
+        Me.btnCalTax.Name = "btnCalTax"
+        Me.btnCalTax.Size = New System.Drawing.Size(259, 82)
+        Me.btnCalTax.TabIndex = 3
+        Me.btnCalTax.Text = "คำนวณ"
+        Me.btnCalTax.UseVisualStyleBackColor = True
+        '
+        'frmJob4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(15.0!, 29.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1012, 533)
+        Me.Controls.Add(Me.btnCalTax)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblAllowance)
+        Me.Controls.Add(Me.lblAllIncome)
+        Me.Controls.Add(Me.lblBonus)
+        Me.Controls.Add(Me.lblYearSalary)
+        Me.Controls.Add(Me.txtSale)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.txtToday)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(8, 7, 8, 7)
+        Me.Name = "frmJob4"
+        Me.Text = "frmJob4"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents txtToday As System.Windows.Forms.TextBox
+    Friend WithEvents txtSalary As System.Windows.Forms.TextBox
+    Friend WithEvents txtSale As System.Windows.Forms.TextBox
+    Friend WithEvents lblYearSalary As System.Windows.Forms.Label
+    Friend WithEvents lblBonus As System.Windows.Forms.Label
+    Friend WithEvents lblAllIncome As System.Windows.Forms.Label
+    Friend WithEvents lblAllowance As System.Windows.Forms.Label
+    Friend WithEvents lblTax As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents radGeneralDate As System.Windows.Forms.RadioButton
+    Friend WithEvents radShortDate As System.Windows.Forms.RadioButton
+    Friend WithEvents radLongDate As System.Windows.Forms.RadioButton
+    Friend WithEvents btnCalTax As System.Windows.Forms.Button
+End Class
---
+++ Lab2/Lab2/frmJob4.vb
@@ -0,0 +1,62 @@
+﻿Public Class frmJob4
+
+ 
+    Dim datetoday As Date = Date.Now
+
+   
+    Private Sub btnCalTax_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalTax.Click
+        Dim salary, YearSalary, Bonus, Allowance, tax, Allincome, sale As Long
+        Const decR_BONUS As Double = 0.2
+        Const decR_ALLOW As Double = 0.01
+        Const decR_TAX As Double = 0.07
+       
+
+        salary = txtSalary.Text
+        txtSalary.Text = FormatNumber(txtSalary.Text, 2)
+        sale = txtSale.Text
+        txtSale.Text = FormatNumber(txtSale.Text, 2)
+
+        YearSalary = salary * 12
+
+        Bonus = sale * decR_BONUS
+
+        Allincome = YearSalary + Bonus
+
+        Allowance = Allincome * decR_ALLOW
+
+        tax = (Allincome - Allowance) * decR_TAX
+
+        lblAllIncome.Text = Allincome
+        lblAllIncome.Text = FormatNumber(lblAllIncome.Text, 2)
+
+        lblAllowance.Text = Allowance
+        lblAllowance.Text = FormatNumber(lblAllowance.Text, 2)
+
+        lblBonus.Text = Bonus
+        lblBonus.Text = FormatNumber(lblBonus.Text, 2)
+
+        lblTax.Text = tax
+        lblTax.Text = FormatNumber(lblTax.Text, 2)
+
+        lblYearSalary.Text = YearSalary
+        lblYearSalary.Text = FormatNumber(lblYearSalary.Text, 2)
+
+
+    End Sub
+
+    Private Sub radShortDate_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles radShortDate.CheckedChanged
+        txtToday.Text = FormatDateTime(datetoday, DateFormat.ShortDate)
+    End Sub
+
+    Private Sub radGeneralDate_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles radGeneralDate.CheckedChanged
+        txtToday.Text = FormatDateTime(datetoday, DateFormat.GeneralDate)
+    End Sub
+
+    Private Sub radLongDate_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles radLongDate.CheckedChanged
+        txtToday.Text = FormatDateTime(datetoday, DateFormat.LongDate)
+    End Sub
+
+    Private Sub frmJob4_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txtToday.Text = FormatDateTime(datetoday, DateFormat.GeneralDate)
+    End Sub
+End Class
